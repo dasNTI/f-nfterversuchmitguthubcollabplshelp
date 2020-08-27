5a6a49c4fca5f92b0e8f8fe6c2dab6797ae0e3c2
dasNTI <69961597+dasNTI@users.noreply.github.com>
2020-08-27 16:11:45 +0200

    p

---
+++ fünfterversuchmitgithubcollabplshelp/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Update is called once per frame
+    
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0, 0.5f, 0, ForceMode.Impulse);
+        }
+    }
+}
