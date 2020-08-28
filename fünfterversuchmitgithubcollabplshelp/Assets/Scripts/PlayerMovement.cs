3fa88f722c0e189b8f766fe149f56534384288e0
dasNTI <69961597+dasNTI@users.noreply.github.com>
2020-08-28 13:34:34 +0200

    p

--- fünfterversuchmitgithubcollabplshelp/Assets/Scripts/PlayerMovement.cs
+++ fünfterversuchmitgithubcollabplshelp/Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,24 @@ public class PlayerMovement : MonoBehaviour
 {
     // Update is called once per frame
     
-    public Rigidbody rb;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    private float xmov = 0;
+    private float zmov = 0;
+    public float Speed = 0.1f;
+
     void Update()
     {
+        transform.position = new Vector3(transform.position.x + xmov * Time.deltaTime, transform.position.y, transform.position.z + zmov * Time.deltaTime);
+        xmov = 0;
+        zmov = 0;
         if (Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(0, 0.5f, 0, ForceMode.Impulse);
+            xmov = Mathf.Sin(Mathf.Abs(transform.eulerAngles.y)) * Speed;
+            zmov = Mathf.Sqrt(Speed*Speed - xmov*xmov);
+        }
+
+        if (Input.GetKey(KeyCode.T)) 
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 1, transform.eulerAngles.z);
         }
     }
 }
