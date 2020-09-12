146d33da6e2f6f97986f45a3e05df885bbacdf6a
dasNTI <69961597+dasNTI@users.noreply.github.com>
2020-09-12 15:18:52 +0200

    p

--- fünfterversuchmitgithubcollabplshelp/Assets/Scripts/PlayerMovement.cs
+++ fünfterversuchmitgithubcollabplshelp/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     // Update is called once per frame
     
-    private float xmov = 0;
-    private float zmov = 0;
+    public float xmov = 0;
+    public float zmov = 0;
     public float Speed = 0.1f;
 
     void Update()
@@ -17,13 +17,14 @@ public class PlayerMovement : MonoBehaviour
         zmov = 0;
         if (Input.GetKey(KeyCode.S))
         {
-            zmov = Mathf.Sin(transform.eulerAngles.y + 180) * Speed;
-            xmov = Mathf.Sqrt(Speed*Speed - zmov*zmov);
+            xmov = Mathf.Sin((transform.eulerAngles.y + 180) % 360) * Speed;
         }
 
         if (Input.GetKey(KeyCode.T)) 
         {
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 1, transform.eulerAngles.z);
         }
+
+        if (transform.eulerAngles.y <= 0) transform.eulerAngles = new Vector3(transform.eulerAngles.x, 360, transform.eulerAngles.z);
     }
 }
