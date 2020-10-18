e2089e1b3a8fb9ec5a4cc2ef2bf9fdced349f016
dasNTI <69961597+dasNTI@users.noreply.github.com>
2020-10-18 18:57:22 +0200

    a<netg

--- fünfterversuchmitgithubcollabplshelp/Assets/Scripts/MouseLook.cs
+++ fünfterversuchmitgithubcollabplshelp/Assets/Scripts/MouseLook.cs
@@ -6,8 +6,7 @@ public class MouseLook : MonoBehaviour
 {
 
     public float mouseSensitivity = 100f;
-
-    public Transform playerBody;
+    private Camera MainCam;
 
     float xRotation = 0f;
 
@@ -15,18 +14,19 @@ public class MouseLook : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        MainCam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        MainCam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
     }
 }
---
+++ fünfterversuchmitgithubcollabplshelp/Assets/Scripts/PlayerWalk.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour
+{
+    public float Speed = 1f;
+    public float mouseSensitivity = 100f;
+    private Camera MainCam;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        MainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Speed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position += transform.right * Speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        MainCam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
